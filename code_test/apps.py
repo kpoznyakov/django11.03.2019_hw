28fd777e9f5e4a3986350ffb802fa7f6cd64ba6b
k.poznyakov <pozkir@gmail.com>
2019-03-23 20:03:02 +0300

    fix

--- code_test/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CodeTestConfig(AppConfig):
-    name = 'code_test'
--- code_test/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import code_test
-
-app_name = 'code_test'
-
-urlpatterns = [
-    path('', code_test, name='main'),
-]
